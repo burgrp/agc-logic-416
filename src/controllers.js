f3f28fe29a1dc754129d2b400a60975726039cda
Pavel Burgr <pb@drake.cz>
2018-11-15 19:05:13 +0100

    WIP

--- src/controllers.js
+++ src/controllers.js
@@ -1,8 +1,9 @@
 const mqttRegister = require("@device.farm/mqtt-reg").mqttReg;
+const deepEqual = require("fast-deep-equal");
 
 module.exports = async config => {
 
-    let allModules = {...config.modules};
+    let allModules = { ...config.modules };
 
     for (let factory of config.factories) {
         await factory(allModules);
@@ -15,15 +16,45 @@ module.exports = async config => {
         let input = {};
         let output = {};
         let outputRegs = {};
+        
+        function executeLogic() {
+            (async () => {
+
+                try {                            
+                    await module.logic(input, output);
+                } finally {
+                    for (let outputName in output) {
+                        let value = output[outputName];
+                        let reg = outputRegs[outputName];
+                        if (reg) {
+                            console.info(`${moduleName}: O ${reg.name} ${value}`);
+                            reg.set(value);
+                        } else {
+                            console.error(`${moduleName} has no output ${outputName}`);
+                        }
+
+                    }                            
+                }
+
+
+            })().catch(e => {
+                console.error(`Error in controller logic ${moduleName}`, e);
+            });
+        }
 
         for (let outputName in module.output) {
             let regName = module.output[outputName];
             outputRegs[outputName] = mqttRegister(config.mqttBroker, regName, actual => {
-                output[outputName] = actual;
+                if (!deepEqual(output[outputName], actual)) {
+                    console.info(`${moduleName}: X ${regName} ${output[outputName]} ${actual}`);                    
+                    output[outputName] = actual;    
+                    executeLogic();
+                }                
             });
             outputRegs[outputName].name = regName;
         }
 
+        // watch also outputs to make sure logic is executed when the output changes externally
         for (let inputName in module.input) {
 
             function createRegister(regName, index) {
@@ -40,23 +71,7 @@ module.exports = async config => {
                         input[inputName][index] = actual;
                     }
 
-                    (async () => {
-                        await module.logic(input, output);
-                    })().catch(e => {
-                        console.error(`Error in controller logic ${moduleName}`, e);
-                    });
-
-                    for (let outputName in output) {
-                        let value = output[outputName];
-                        let reg = outputRegs[outputName];
-                        if (reg) {
-                            console.info(`${moduleName}: O ${reg.name} ${value}`);
-                            reg.set(value);
-                        } else {
-                            console.error(`${moduleName} has no output ${outputName}`);
-                        }
-
-                    }
+                    executeLogic();
 
                 });
             }
