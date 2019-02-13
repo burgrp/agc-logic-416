94554d1ddab49394e7ff60e3b6ab4f742acf41e9
Pavel Burgr <pb@drake.cz>
2019-02-13 10:49:38 +0100

    WIP

--- src/hot-water-by-heating.js
+++ src/hot-water-by-heating.js
@@ -1,8 +1,9 @@
 module.exports = config => {
     return function (input, output) {
         if (output.pump !== undefined) {
+            let heatingTemp = Math.max(input.accuTemp, input.heatPumpOutTemp);
             output.pump =
-                input.heatingTemp > input.hotWaterTemp + 5 &&
+                heatingTemp > input.hotWaterTemp + 5 &&
                 input.hotWaterTarget > input.hotWaterTemp + (output.pump ? -1 : 1);
         }
     }
