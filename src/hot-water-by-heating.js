1704993dfe208c6413b53980f84d4a8b2697e04c
Pavel Burgr <pb@drake.cz>
2019-11-01 12:14:46 +0100

    WIP

--- src/hot-water-by-heating.js
+++ src/hot-water-by-heating.js
@@ -2,7 +2,7 @@ module.exports = config => {
     return function (input, output) {
         if (output.pump !== undefined) {
             let heatingTemp = Math.max(input.accuTemp || 0, input.heatPumpOutTemp || 0);
-            let actualToCompare = input.hotWaterTemp + (output.pump ? -1 : 1);
+            let actualToCompare = input.hotWaterTemp + (output.pump ? -2 : 2);
             output.pump =
                 input.enabled &&
                 heatingTemp > actualToCompare + 8
