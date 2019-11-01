3300ecf81bf8071ed0e4d458f68d7e8f44088c23
Pavel Burgr <pb@drake.cz>
2019-11-01 13:04:43 +0100

    WIP

--- src/hot-water-by-heating.js
+++ src/hot-water-by-heating.js
@@ -1,8 +1,14 @@
-module.exports = config => {
+
+let lastToCompare;
+// lastToCompare - temporary workaround for unreliable hotWaterBoiler device
+
+module.exports = config => {    
     return function (input, output) {
 
         let heatingTemp = Math.max(input.accuTemp || 0, input.heatPumpOutTemp || 0);
-        let actualToCompare = input.hotWaterTemp + (output.pump ? -2 : 2);
+        let actualToCompare = (input.hotWaterTemp + (output.pump ? -2 : 2)) || lastToCompare;
+        lastToCompare = actualToCompare;
+        
         output.pump =
             input.enabled &&
             heatingTemp > actualToCompare + 5
